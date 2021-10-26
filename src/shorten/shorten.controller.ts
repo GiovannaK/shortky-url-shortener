7ba96df3965b8e98d15a2543a32a5161daecd8f0
Giovanna Cunha <giovannakelli@gmail.com>
2021-10-26 01:39:26 -0300

    status code info was added to swagger

--- src/shorten/shorten.controller.ts
+++ src/shorten/shorten.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { ShortenService } from './shorten.service';
@@ -13,6 +13,11 @@ export class ShortenController {
   @ApiOperation({
     summary: 'Receive a originURL, store on database and return ShortURL',
   })
+  @ApiResponse({ status: 201, description: 'ShortURL created successfully' })
+  @ApiResponse({
+    status: 500,
+    description: 'Cannot create shortURL, server error',
+  })
   async shortenUrl(@Body() createUrlDto: CreateUrlDto) {
     return await this.shortenService.shortenUrl(createUrlDto);
   }
@@ -21,6 +26,14 @@ export class ShortenController {
   @ApiOperation({
     summary:
       'Receive a hash param, check if param exist on database and redirect to link',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'URL exist and user will be redirect',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Cannot found URL',
   })
   async redirect(@Param('hash') hash: string, @Res() response: Response) {
     return await this.shortenService.redirect(hash, response);
